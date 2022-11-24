d46e8dd7c97715eb366bdfcfbb5b7b03fbfdf5d3
phadalgekar <hadalgekarprathamesh@gmail.com>
2022-11-24 17:49:29 +0530

    Start position 0

---
+++ StartingPos.java
@@ -0,0 +1,13 @@
+package snakeLadder;
+
+public class StartingPos {
+	public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("Welcome to Snake And Ladder game");
+        System.out.println("player start position is " + pos);
+
+    }
+
+}
+
+	
